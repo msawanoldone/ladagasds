82e4506146550fb874d717d5d18dfdc004b18975
jsnjfndsfdsasd <40805903+ujhsfjhfsfa@users.noreply.github.com>
2019-02-24 03:12:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const moment = require('moment');
 const Util = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
